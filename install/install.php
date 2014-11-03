46187dd9567ddb0e9ad28f6a32f43b062f701e69
Michael Karbowiak <michael@karbowiak.dk>
2014-11-03 19:45:37 +0100

    Forgot to readd the modified installer, oops

--- install/install.php
+++ install/install.php
@@ -182,7 +182,7 @@ if($ln == true)
 
 // move cron.overrides to main dir
 // Save the file and then attempt to load and initialize from that file
-$cronoverridesLoc = "$base/../cron.overrides";
+$cronoverridesLoc = "$base/../cronoverrides";
 $cronOverrides = file_get_content("$base/cron.overrides");
 if (file_put_contents($cronoverridesLoc, $cronOverrides) === false)
 	out("|r|Unable to write cron.overrides at $cronoverridesLoc", true);
