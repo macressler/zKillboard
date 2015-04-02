a43fbb44bc1c04a3c555f3e6aa220ea1419021c2
Clinton Weiss <cvweiss@gmail.com>
2015-04-02 03:37:11 +0000

    removed the sleeps, the crest calls take long enough on their own

--- cli/cli_wars.php
+++ cli/cli_wars.php
@@ -100,12 +100,6 @@ class cli_wars implements cliCommand
 							":openForAllies" => $warInfo->openForAllies,
 							":warID" => $id,
 							));
-				$diff = $timer->stop() - $now;
-				if ($diff < 200)
-				{
-					$sleep = 200 - $diff;
-					usleep(1000 * $sleep);
-				}
 			}
 		}
 		if ($added > 0) Log::log("CREST (war): Added $added killmails");
